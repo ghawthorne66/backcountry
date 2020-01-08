f1263bdd538bb113286335005466d48f01424794
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-01-08 13:53:29 -0800

    add layout.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-
- const Layout = ({children}) => {
-    return (
-  <>
-  <Navbar />
-  {children}
-  <Footer />
-  </>
-    )
+import React from "react"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import "./layout.css"
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
