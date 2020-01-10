926f726d3f59146bbd0aba0b942ff6faf55f717d
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-01-09 16:18:28 -0800

    add graphql

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
-import Button from "../examples/Button"
+import Header from "../examples/Header"
+
 export default class tours extends Component {
   render() {
     return (
       <Layout>
         hello from tours page!!!!!
+        <Header />
        
       </Layout>
     )
