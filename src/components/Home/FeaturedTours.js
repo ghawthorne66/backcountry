0b408545be0241ecbeee96dfb85317ed92c55664
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-01-10 17:38:26 -0800

    add contentful

---
+++ src/components/Home/FeaturedTours.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Tour from "../Tours/Tours"
+import { useStaticQuery, graphql } from "gatsby"
+import Title from "../Title"
+import styles from "../../css/items.module.css"
+import AniLink from "gatsby-plugin-transition-link"
+
+const getTours = graphql`
+  query {
+    featuredTours: allContentfulTour(filter: { featured: { eq: false} }) {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
+const FeaturedTours = () => {
+    const response = useStaticQuery(getTours)
+    const tours = response.featuredTours.edges
+    console.log(tours)
+    
+  return (
+    <section className={styles.tours}>
+      <Title title="featured" subtitle="tours" />
+      hello from featured tours
+      <Tour />
+      <AniLink fade to="/tours" className="btn-primary">
+        all Tours
+      </AniLink>
+    </section>
+  )
+}
+
+export default FeaturedTours
---
+++ src/components/Tours/Tours.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tours = () => {
+    return (
+        <div>
+            this is a single tour
+        </div>
+    )
+}
+
+export default Tours
+
