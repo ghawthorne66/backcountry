d293b77c5ddd29a1a6b3be4132c139418809034f
greg hawthorne <apple@Apples-MacBook-Air.local>
2020-01-12 16:55:04 -0800

    add seo components to all pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+      }
+    },
       {
         resolve: 'gatsby-plugin-robots-txt',
         options: {
